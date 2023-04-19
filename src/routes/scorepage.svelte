ff91650f4b04a167b9084905b62c45e99d28b9d9
Will <willtrentphillips@gmail.com>
2023-04-19 12:29:03 +1000

    Baasic styling of score page

--- src/routes/scorepage.svelte
+++ src/routes/scorepage.svelte
@@ -4,7 +4,12 @@
 </script>
 
 <style>
-
+    h2 {
+        text-align: center;
+        font-size: 28px;
+        font: sans-serif;
+        font-weight: bold;
+    }
 </style>
 
 <h2>You Scored {score}/{questionslength}</h2>
