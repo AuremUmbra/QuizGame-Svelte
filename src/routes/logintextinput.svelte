7ba841c9c4aa7e8d279e0526783689d36e9399eb
Will <willtrentphillips@gmail.com>
2023-04-20 11:47:25 +1000

    Comments for logintextinput component

--- src/routes/logintextinput.svelte
+++ src/routes/logintextinput.svelte
@@ -1,11 +1,14 @@
 <script>
+    //Defining and exporting variables
     export let placeholder = input;
     export let value;
 </script>
 <style>
+    /* Aligning text to center of input */
     .login {
         text-align:center;
     }
 </style>
 
+<!-- Creating text input login for username  -->
 <input class="login" type="text" bind:value placeholder={placeholder}>
