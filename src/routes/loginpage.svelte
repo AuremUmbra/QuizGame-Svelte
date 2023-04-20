9fa46da97d1d4d630e9148e1b35efcd2f32d5e2f
Will <willtrentphillips@gmail.com>
2023-04-20 11:38:10 +1000

    modify comments for loginpage component

--- src/routes/loginpage.svelte
+++ src/routes/loginpage.svelte
@@ -1,11 +1,16 @@
 <script>
+    // Importing components
     import Loginbutton from "./loginbutton.svelte";
     import Logintextinput from "./logintextinput.svelte";
     import Passwordtextinput from "./passwordtextinput.svelte";
     import Title from "./title.svelte";
-    import Logos from "./Logos.svelte"
+    import Logos from "./Logos.svelte";
+    // Defining and exporting variables to be defined in importing components
     export let username;
     export let password;
+
+    // Moved to different component
+
     // export let admin_visibility = 0;
     // export let quiz_visibility = 0;
     // function handleLogIn() {
@@ -23,20 +28,23 @@
 </script>
 
 <style>
+    /* Centering and margining login area */
     .login {
         display: flex;
         justify-content: center;
         margin-top: 80px;
     }
-
+    /* Giving background color for login page */
     body {
         background-color:  #298fbf;
     }
 </style>
 
+<!-- Getting Title and Logos -->
 <Title/>
 <Logos/>
 <body>
+    <!-- Creating form for password, username and login button -->
     <form class = "login">
         <Logintextinput placeholder="Username" bind:value={username}/>
         <Passwordtextinput placeholder="Password" bind:value={password}/>
