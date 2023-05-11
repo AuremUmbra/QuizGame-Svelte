62efdb1ccf4ddf405cb0f7eb2dfa81b53787f0b8
Will <willtrentphillips@gmail.com>
2023-05-11 13:50:15 +1000

    Getting the login connection to API ready - not in use yet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,8 @@
     let tempusername = 'Admin';
     let temppassword = 'password';
 
-    function handleLogIn() {
+    async function handleLogIn() {
+        //----This is Temporary - Remove When Proper Login Available
         if (username === tempusername && password === temppassword) {
             username = "";
             password = "";
@@ -39,7 +40,33 @@
             username="";
             password="";
         }
-        
+        //----End Temporary Login
+
+        //----Start Future Login - UnComment When Done.
+
+        // const login = await fetch("https://dtpkanganquestionapi.azurewebsites.net/CheckUser", {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         username,
+        //         password
+        //     })
+        // })
+        // const json = await login.json()
+        // if (json == true && username == tempusername) {
+        //     username = "";
+        //     password = "";
+        //     admin_visibility = 1;
+        // } else if (json == true) {
+        //     username="";
+        //     password="";
+        //     quiz_visibility = 1;
+        // } else {
+        //     alert("Incorrect username or password");
+        //     username="";
+        //     password="";
+        // }
+
+        //----End Future Login
     }
 
     function handleLogOut() {
